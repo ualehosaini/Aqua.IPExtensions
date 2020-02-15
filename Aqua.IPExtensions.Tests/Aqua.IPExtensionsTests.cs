bf281a1981fdeb7dce5c9486461349547a612853
ualehosaini2019 <ualehosaini2019@hotmail.com>
2020-02-15 04:08:51 +0000

    IsValidIPv4 has been added

--- Aqua.IPExtensions.Tests/Aqua.IPExtensionsTests.cs
+++ Aqua.IPExtensions.Tests/Aqua.IPExtensionsTests.cs
@@ -4,10 +4,34 @@ namespace Aqua.IPExtensions.Tests
 {
     public class IPExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("192.168.4.254")]
+        [InlineData("192.168.4.55")]
+        [InlineData("192.168.4.1")]
+        [InlineData("191.168.5.254")]
+        [InlineData("191.168.5.33")]
+        [InlineData("191.168.5.1")]
+        [InlineData("10.0.0.254")]
+        [InlineData("10.0.0.1")]
+        [InlineData("169.254.1.1")]
+        [InlineData("127.0.0.1")]
+        [InlineData("0.0.0.0")]
+        [InlineData("7")]
+        public void IsValidIPv4_Valid(string ip)
         {
+            Assert.True(ip.IsValidIPv4());
+        }
 
+        [Theory]
+        [InlineData("259.168.4.254")]
+        [InlineData("-1.168.4.55")]
+        [InlineData("444.168.4.1")]
+        [InlineData("-11.-99.9.1")]
+        [InlineData("Foo")]
+        [InlineData("127.0.0.333")]
+        public void IsValidIPv4_InValid(string ip)
+        {
+            Assert.False(ip.IsValidIPv4());
         }
     }
 }
--- Aqua.IPExtensions/Aqua.IPExtensions.cs
+++ Aqua.IPExtensions/Aqua.IPExtensions.cs
@@ -1,6 +1,29 @@
-﻿namespace Aqua.IPExtensions
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Aqua.IPExtensions
 {
     public static class IPExtensions
     {
+        /// <summary>
+        /// To validate an IPv4 string
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsValidIPv4(this string ip)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                return false;
+            }
+
+            IPAddress ipTemp;
+            if (IPAddress.TryParse(ip, out ipTemp))
+            {
+                return ipTemp.AddressFamily == AddressFamily.InterNetwork;
+            }
+
+            return false;
+        }
     }
 }
